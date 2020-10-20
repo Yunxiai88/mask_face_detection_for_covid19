71ac16394886fc4b2b51b50850275bfbf98e8cd6
NormanLYJ <leeyuejun@gmail.com>
2020-10-20 21:16:25 +0800

    remove mtcnn dependency

--- webApp/models/facenet.py
+++ webApp/models/facenet.py
@@ -4,7 +4,6 @@ import csv
 import numpy as np
 
 from PIL import Image
-from mtcnn.mtcnn import MTCNN
 
 from sklearn.preprocessing import Normalizer
 from scipy.spatial.distance import cosine
@@ -206,34 +205,34 @@ class FaceNet:
     #             cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1.5, color, 10)
 
     # use MTCNN to detect faces and return face array
-    def extract_mtcnn_face(self, filename, required_size=(160, 160)):
-        print("extracting face from image")
-        detector = MTCNN()
+    # def extract_mtcnn_face(self, filename, required_size=(160, 160)):
+    #     print("extracting face from image")
+    #     detector = MTCNN()
 
-        image = Image.open(filename)
-        # convert to RGB, if needed
-        image = image.convert('RGB')
-        # convert to array
-        pixels = np.asarray(image)
-        # detect faces in the image
-        results = detector.detect_faces(pixels)
-        # extract the bounding box from the first face
-        x1, y1, width, height = results[0]['box']
-        # deal with negative pixel index
-        x1, y1 = abs(x1), abs(y1)
-        x2, y2 = x1 + width, y1 + height
-        # extract the face
-        face = pixels[y1:y2, x1:x2]
-        # resize pixels to the model size
-        image = Image.fromarray(face)
-        image = image.resize(required_size)
-        basename = os.path.splitext(filename)[0]
-        outputfile = basename+"_face.jpg"
-        cv2.imwrite(utils.get_file_path('webApp/uploads', outputfile), image)
+    #     image = Image.open(filename)
+    #     # convert to RGB, if needed
+    #     image = image.convert('RGB')
+    #     # convert to array
+    #     pixels = np.asarray(image)
+    #     # detect faces in the image
+    #     results = detector.detect_faces(pixels)
+    #     # extract the bounding box from the first face
+    #     x1, y1, width, height = results[0]['box']
+    #     # deal with negative pixel index
+    #     x1, y1 = abs(x1), abs(y1)
+    #     x2, y2 = x1 + width, y1 + height
+    #     # extract the face
+    #     face = pixels[y1:y2, x1:x2]
+    #     # resize pixels to the model size
+    #     image = Image.fromarray(face)
+    #     image = image.resize(required_size)
+    #     basename = os.path.splitext(filename)[0]
+    #     outputfile = basename+"_face.jpg"
+    #     cv2.imwrite(utils.get_file_path('webApp/uploads', outputfile), image)
 
-        face_array = np.asarray(image)
+    #     face_array = np.asarray(image)
 
-        return face_array
+    #     return face_array
     def extract_face(self, filename):
         filepath = utils.get_file_path('webApp/uploads', filename)
         image = cv2.imread(filepath)
